853db00a34ef38760598ef080013a75f65237f40
Izaac Morales <maccabee@Izaacs-MacBook-Pro.local>
2017-05-09 23:10:46 -0400

    everything but cmp coded and ready to test

--- dataproc.py
+++ dataproc.py
@@ -2,10 +2,15 @@
 Data processing instructions
 """
 
+from sys import argv
 from instr import Instr
-from simparts import simCPU, simRAM
+from simparts import simCPU
 
 class DataProc(Instr):
+    """
+    Class for Data Processing Instructions
+    Inherits from Instr class
+    """
     def __init__(self, bits):
         Instr.__init__(self, bits)
 
@@ -26,6 +31,18 @@ class DataProc(Instr):
         self.is_imm_shift = False
         self.is_reg_shift = False
         self.result = 0
+
+#function dictionary that matches opcodes to the data processing instruction functions
+    i_instrs = {0b0000: i_and,
+                0b0001: i_eor,
+                0b0010: i_sub,
+                0b0011: i_rsb,
+                0b0100: i_add,
+                0b1010: i_cmp,
+                0b1100: i_orr,
+                0b1101: i_mov,
+                0b1110: i_bic,
+                0b1111: i_mvn}
 
 #decoding methods
     def get_opcode(self):
@@ -60,96 +77,127 @@ class DataProc(Instr):
             self.rm = (self.bits & 0xf)
             self.is_reg_shift = True
 
-#methods that affect the <result>
+#method that affects <result>
     def set_result(self):
-        """change <result> according to shifts to be written to <rd>"""
+        """change <result> according to shifts"""
+        #rotating right to create immediate value
         if self.is_32bit:
-            rot_bits = self.imm_8 & self.make_mask(self.rot_imm*2) << (31-self.rot_imm*2)
+            rot_bits = (self.imm_8 & self.make_mask(self.rot_imm*2)) << (31-self.rot_imm*2)
             self.result = (self.imm_8 >> (self.rot_imm * 2)) | rot_bits
+        #shifting by immediate value
         elif self.is_imm_shift:
             #logical shift left
-            if self.shift == 0x00:
+            if self.shift == 0b00:
                 self.result = simCPU.get_reg(self.rm) << self.shift_imm
             #logical shift right
-            elif self.shift == 0x01:
+            elif self.shift == 0b01:
                 self.result = simCPU.get_reg(self.rm) >> self.shift_imm
-                self.result &= self.make_mask(self.shift_imm) << (31 - self.shift_imm)
+                self.result &= self.make_mask(31 - self.shift_imm)
             #arithmetic shift right
-            elif self.shift == 0x10:
+            elif self.shift == 0b10:
+                #retrieve source value
                 val = simCPU.get_reg(self.rm)
+                #MSB is set
                 if val >> 31 == 1:
-                    self.result = (val >> self.shift_imm)
-                    self.result |= (self.make_mask(self.shift_imm) << (31 - self.shift_imm))
+                    #shift source value
+                    self.result = val >> self.shift_imm
+                    #set shifted in bits
+                    self.result |= self.make_mask(self.shift_imm) << (31 - self.shift_imm)
+                #MSB is not set (basically a logical shift right)
+                else:
+                    #shift source value
+                    self.result = val >> self.shift_imm
+                    #clear shifted in bits
+                    self.result &= self.make_mask(31 - self.shift_imm)
+            elif self.shift == 0b11:
+                pass
+        #shifting by value found in <rs>
         elif self.is_reg_shift:
             #logical shift left
-            if self.shift == 0x00:
+            if self.shift == 0b00:
                 self.result = simCPU.get_reg(self.rm) << simCPU.get_reg(self.rs)
             #logical shift right
-            elif self.shift == 0x01:
-                mask = self.make_mask(simCPU.get_reg(self.rs))
+            elif self.shift == 0b01:
                 self.result = simCPU.get_reg(self.rm) >> simCPU.get_reg(self.rs)
-                self.result &= mask << (31 - (simCPU.get_reg(self.rs) * 2))
+                self.result &= self.make_mask(31 - simCPU.get_reg(self.rs))
             #arithmetic shift right
-            elif self.shift == 0x10:
+            elif self.shift == 0b10:
+                res_val = simCPU.get_reg(self.rm)
+                shift_val = simCPU.get_reg(self.rs)
+                if res_val >> 31 == 1:
+                    #shift source value
+                    self.result = res_val >> shift_val
+                    #set shifted in bits
+                    self.result |= self.make_mask(shift_val) << (31 - shift_val)
+                else:
+                    #shift source value
+                    self.result = res_val >> shift_val
+                    #clear shifted in bits
+                    self.result &= self.make_mask(31 - shift_val)
+            elif self.shift == 0b11:
                 pass
 
 #the actual data processing instructions
     def i_mov(self):
-        """write a value to rd"""
+        """write a value to <rd>"""
         simCPU.set_reg(self.rd, self.result)
 
     def i_mvn(self):
-        """writes the logical not of a value to rd"""
-        pass
+        """write the logical not of a value to <rd>"""
+        simCPU.set_reg(self.rd, ~self.result)
 
     def i_add(self):
         """adds two values together and stores the result in rd"""
-        pass
+        simCPU.set_reg(self.rd, simCPU.get_reg(self.rn) + self.result)
 
     def i_sub(self):
         """
         subtracts two values and stores the result in rd
-        first value from a register
+        first operand from a register
         """
-        pass
+        simCPU.set_reg(self.rd, simCPU.get_reg(self.rn) - self.result)
 
     def i_rsb(self):
         """
         subtracts two values and stores the result in rd
         second value from a register
         """
-        pass
+        simCPU.set_reg(self.rd, self.result - simCPU.get_reg(self.rn))
 
     def i_mul(self):
-        pass
+        """
+        multiply 2 values and store result in <rd>
+        (rd bits where rn bits typically are in a data processing instruction)
+        """
+        simCPU.set_reg(self.rn, simCPU.get_reg(self.rm) * simCPU.get_reg(self.rs))
 
     def i_and(self):
-        pass
+        """perform a bitwise AND of two values and store result in <rd>"""
+        simCPU.set_reg(self.rd, simCPU.get_reg(self.rn) & self.result)
 
     def i_orr(self):
-        pass
+        """perform a bitwise OR of two values and store result in <rd>"""
+        simCPU.set_reg(self.rd, simCPU.get_reg(self.rn) & self.result)
 
     def i_eor(self):
-        pass
+        """perform a bitwise XOR of two values and store result in <rd>"""
+        simCPU.set_reg(self.rd, simCPU.get_reg(self.rn) ^ self.result)
 
     def i_bic(self):
-        pass
+        """
+        perform a bitwise AND of one value with the complement of another
+        store the result in <rd>
+        """
+        simCPU.set_reg(self.rd, simCPU.get_reg(self.rn) & ~self.result)
 
     def i_cmp(self):
         pass
 
-#pythonese switch-case helper thing
-    i_instrs = {0b0000: i_and,
-                0b0001: i_eor,
-                0b0010: i_sub,
-                0b0011: i_rsb,
-                0b0100: i_add,
-                0b1010: i_cmp,
-                0b1100: i_orr,
-                0b1101: i_mov,
-                0b1110: i_bic,
-                0b1111: i_mvn}
-
+#method to execute the decoded instruction
     def i_execute(self):
         self.set_result()
         self.i_instrs[self.opcode]()
+
+if __name__ == "__main__":
+    test_instr = DataProc(int(argv[1]))
+    test_instr.i_execute()

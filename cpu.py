ae2bd31036397910c4cac354e87c22a64fc34ce4
Izaac Morales <maccabee@Izaacs-MacBook-Pro.local>
2017-05-12 01:00:05 -0400

    got rid of unnecessary cruft

--- cpu.py
+++ cpu.py
@@ -4,7 +4,7 @@ Where the registers live and all methods for register
     access/modification exist
 """
 
-from simparts import simRAM
+from instr import Instr
 
 class CPU:
     """The CPU class"""
@@ -12,18 +12,10 @@ class CPU:
         self.regs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.instr_count = 0
 
-    def get_reg(self, regnum):
-        """return the value in register <regnum>"""
-        return self.regs[regnum]
-
-    def set_reg(self, regnum, val):
-        """set value in register <regnum> to <val>"""
-        self.regs[regnum] = val
-
     def fetch(self):
         """retrieves the 32-bit instruction at the address contained in pc (i.e. regs[15])"""
         self.instr_count += 1
-        return simRAM.read_mem(self.regs[15])
+        return 0
 
     def decode(self):
         """decode the instruction at address retrieved from pc"""
--- instr.py
+++ instr.py
@@ -5,8 +5,10 @@ Generic instruction class from which others inherit
 class Instr:
     def __init__(self, bits):
         self.bits = bits
-        self.rn = 0
-        self.rd = 0
+        #source register
+        self.rn = self.bits >> 16 & 0xf
+        #destination register number
+        self.rd = self.bits >> 12 & 0xf
 
     def make_mask(self, num_ones):
         """create a mask of 1s length <num_ones>"""
@@ -15,17 +17,19 @@ class Instr:
             res |= (1 << i)
         return res
 
-    def get_rn(self):
-        """determine the source register number"""
-        self.rn = (self.bits >> 16 & 0xf)
-
-    def get_rd(self):
-        """determine the destination register number"""
-        self.rd = (self.bits >> 12 & 0xf)
+    def check_mul(self):
+        """
+        check bits 25, 7, and 4 to see if the instruction
+        is masquerading as a MUL instruction
+        """
+        if self.bits >> 25 & 1 == 0:
+            if self.bits >> 7 & 1 == 1:
+                if self.bits >> 4 & 1 == 1:
+                    return True
 
     def i_execute(self):
         """
         generic instruction execution method to be
-        filled in by child classes
+        filled in by child instruction classes
         """
         pass

64fb9a884181771c2a8b1e46f460dfe575c72c38
imora310 <imora310@students.bju.edu>
2017-05-21 21:02:07 -0400

    thinking about using tuples instead of lists for some of the storage within CPU and RAM

--- ram.py
+++ ram.py
@@ -5,7 +5,7 @@ Where all the memory of the elf file exists
 class RAM:
     """The memory of the system"""
     def __init__(self, mem_size):
-        self.memory = []
+        self.memory = (0, ) * mem_size
 
 if __name__ == "__main__":
     pass
